b87c71dc5db9bea9f4d765fc2c8e7a18e16d7fed
Yash Adukia <adukia.yash@gmail.com>
2021-05-05 00:23:20 -0700

    content change

--- Java/6_M_ZigZag_Conversion.java
+++ Java/6_M_ZigZag_Conversion.java
@@ -1,12 +1,3 @@
-/*
-The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
-(you may want to display this pattern in a fixed font for better legibility)
-
-P   A   H   N
-A P L S I I G
-Y   I   R
-And then read line by line: "PAHNAPLSIIGYIR"
-*/
 
 class Solution {
     public String convert(String s, int numRows) {
